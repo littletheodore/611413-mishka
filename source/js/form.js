00b08492b99e15f5b475ba11f59abd8c106d8000
Littletheodore <balandinamo@gmail.com>
2018-10-23 22:21:53 +0300

    Добавлены адаптивные декоративные элементы

--- source/js/form.js
+++ source/js/form.js
@@ -1,10 +1,24 @@
-var button = document.querySelector (".page-header__menu--button");
-var user_list = document.querySelector (".page-header__user-list");
-var nav_list = document.querySelector (".page-header__nav-list");
+var button = document.querySelector(".page-header__menu--button");
+var user_list = document.querySelector(".page-header__user-list");
+var nav_list = document.querySelector(".page-header__nav-list");
 
-button.addEventListener("click", function(evt) {
-evt.preventDefault();
-user_list.classList.toggle("page-header_list--on");
-nav_list.classList.toggle("page-header_list--on");
-button.classList.toggle("button-off");
+var button_pop = document.querySelector(".promo-item__buy");
+var popup = document.querySelector(".popup");
+
+
+button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    user_list.classList.toggle("page-header_list--on");
+    nav_list.classList.toggle("page-header_list--on");
+    button.classList.toggle("button-off");
+});
+
+button_pop.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("popup-off");
+});
+
+popup.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("popup-off");
 });
