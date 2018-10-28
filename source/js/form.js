c5ab0c7b53f28f6db09c44a330e962faa29676e4
Littletheodore <balandinamo@gmail.com>
2018-10-28 16:33:39 +0300

    Сдаем

--- source/js/form.js
+++ source/js/form.js
@@ -1,15 +1,19 @@
 var button = document.querySelector(".page-header__menu--button");
 var user_list = document.querySelector(".page-header__user-list");
 var nav_list = document.querySelector(".page-header__nav-list");
-
+var menu = document.querySelector(".page-header__nav-list");
 var button_pop = document.querySelector(".promo-item__buy");
 var popup = document.querySelector(".popup");
 
+if(!menu) {
+  user_list.classList.add("page-header_list--off");
+  nav_list.classList.add("page-header_list--off");
+}
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  user_list.classList.toggle("page-header_list--on");
-  nav_list.classList.toggle("page-header_list--on");
+  user_list.classList.toggle("page-header_list--off");
+  nav_list.classList.toggle("page-header_list--off");
   button.classList.toggle("button-off");
 });
 
